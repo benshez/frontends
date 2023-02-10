6bb4e0a4d1d3a73062d526815ee0f33d8d0e919b
YOUI\benvh01 <benvh@youi.com>
2023-02-10 18:15:13 +1000

    Updated menu

--- shezzy.frontend/components/TopBar.vue
+++ shezzy.frontend/components/TopBar.vue
@@ -6,8 +6,6 @@
 
     <template #end>
       <div v-if="user" class="flex align-items-center">
-        <p class="mr-2">Welcome, {{ user.nickname }} | <a href="/api/auth/logout">Logout</a>
-        </p>
         <Avatar :image="user.picture" shape="circle" />
       </div>
     </template>
@@ -17,27 +15,13 @@
 <script setup lang="ts">
 const user = useUser()
 
-const onLoginClick = async () => {
-  await useFetch<any>('/api/auth/login', { server: false })
-}
-
-const onLogoutClick = async () => {
-  await useFetch<any>('/api/auth/logout', { server: true })
-}
-
 const items = [
-  {
-    label: `Welcome, ${user?.value?.nickname}`,
-    icon: 'pi pi-fw pi-home',
-    url: '/api/auth/logout',
-    visible: user.value != null
-  },
   {
     label: `Login`,
     icon: 'pi pi-fw pi-home',
     url: '/api/auth/login',
     visible: user.value === null
-  },  
+  },
   {
     label: 'Home',
     icon: 'pi pi-fw pi-home',
@@ -59,7 +43,19 @@ const items = [
     icon: 'pi pi-fw pi-user',
     to: '/survey',
     visible: user.value != null
-  }
+  },
+  {
+    label: `Welcome, ${user?.value?.nickname}`,
+    icon: 'pi pi-fw pi-home',
+    visible: user.value != null,
+    items: [
+      {
+        label: 'Logout',
+        icon: 'pi pi-fw pi-home',
+        url: '/api/auth/logout',
+      }
+    ]
+  },
 ]
 </script>
 
